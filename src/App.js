2e5687ae7e965fa38facb69020ec4d544ec4d1c3
jimmaclean <jqim87@gmail.com>
2020-09-10 21:12:47 +0100

    add basic api call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+const getAllBreeds = () => {
+  return fetch('https://dog.ceo/api/breeds/list/all')
+    .then(response => response.json());
+}
+
+class App extends Component {
+  componentDidMount() {
+    getAllBreeds().then(({message}) => console.log(message))
+  }
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
-  );
+    )
+
+  }
 }
 
 export default App;
